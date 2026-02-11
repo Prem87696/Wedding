4a66430086c2641393135f22867207ffe3130954
PREM <prem92006@gmail.com>
2026-02-11 23:48:54 +0530

    Fix comment syntax in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: [
@@ -44,4 +44,3 @@ module.exports = {
   },
   plugins: [],
 }
-s
