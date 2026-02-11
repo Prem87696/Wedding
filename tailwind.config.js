07a209d839fe1284b5ce23e016409d731ae2ba00
PREM <prem92006@gmail.com>
2026-02-11 22:45:11 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        border: "var(--border)",
+        ring: "var(--ring)",
+      }
+    }
+  },
+  plugins: [],
+}
